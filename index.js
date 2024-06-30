ab41c5d9d07e296fbee7e3d3cff13c7191532af7
FedYou <federicochurruarin0@gmail.com>
2024-06-29 23:28:35 -0300

    Import error

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let write = require("./src/write/index");
-let read = require("./src/read/index");
 let remove = require("./src/remove");
 let copy = require("./src/copy");
 let move = require("./src/move");
