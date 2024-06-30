2c97b423029f05bd1114be0a97b59c13f2f319f6
FedYou <federicochurruarin0@gmail.com>
2024-06-29 21:43:33 -0300

    Repair of broken imports

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ let remove = require("./src/remove");
 let copy = require("./src/copy");
 let move = require("./src/move");
 module.exports = {
-  write,
-  read,
+  write: {
+    dir: require("./src/write/dir"),
+    file: require("./src/write/file"),
+    json: require("./src/write/json"),
+  },
+  read: {
+    dir: require("./src/read/dir"),
+    file: require("./src/read/file"),
+    json: require("./src/read/json"),
+    json5: require("./src/read/json5"),
+  },
   remove,
   copy,
   move,
--- src/write/index.js
+++
@@ -1,9 +0,0 @@
-const file = require("./file");
-const json = require("./json");
-const dir = require("./dir");
-
-module.exports = {
-  file,
-  dir,
-  json,
-};
