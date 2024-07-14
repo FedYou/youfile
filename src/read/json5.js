439088084033b74b2e90014a14c5d1dc3c5aac3d
FedYou <federicochurruarin0@gmail.com>
2024-07-14 19:26:29 -0300

    Solution in json5.

--- src/read/json5.js
+++ src/read/json5.js
@@ -1,10 +1,17 @@
 const file = require("./file");
-const regex = /\/\/.*|\/\*[\s\S]*?\*\//g;
+const regex = /("([^"\\]|\\.)*"|'([^'\\]|\\.)*'|\/\/.*|\/\*[\s\S]*?\*\/)/g;
 /**
  * Returns the contents of the file in an object, allows reading json with comments.
  * @param {string} path - File path.
  * @returns {object}
  */
 module.exports = (path) => {
-  return JSON.parse(file(path).replace(regex, ""));
+  return JSON.parse(
+    file(path).replace(regex, (match) => {
+      if (match.startsWith('"') || match.startsWith("'")) {
+        return match;
+      }
+      return "";
+    })
+  );
 };
