6be789ddfd8abf666530bae409055f45c880ab68
FedYou <federicochurruarin0@gmail.com>
2024-08-16 00:30:55 -0300

    Corrected the reading of json5 and json with the “Byte Order Mark” (BOM)

--- src/read/json.js
+++ src/read/json.js
@@ -1,10 +1,27 @@
 const file = require("./file");
-
+const regex = /("([^"\\]|\\.)*"|'([^'\\]|\\.)*'|\/\/.*|\/\*[\s\S]*?\*\/)/g;
 /**
- * Returns the contents of the file as a object.
+ * Returns the contents of the file in an object, allows reading json with comments.
  * @param {string} path - File path.
  * @returns {object}
  */
 module.exports = (path) => {
-  return JSON.parse(file(path));
+  try {
+    let content = file(path);
+    if (content.charCodeAt(0) === 0xfeff) {
+      content = content.slice(1);
+    }
+
+    return JSON.parse(
+      content.replace(regex, (match) => {
+        if (match.startsWith('"') || match.startsWith("'")) {
+          return match;
+        }
+        return "";
+      })
+    );
+  } catch (error) {
+    console.log(path);
+    console.log(error);
+  }
 };
