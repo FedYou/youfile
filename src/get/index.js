318eb517f176df63a31dead59421988070699970
FedYou <federicochurruarin0@gmail.com>
2024-11-14 01:34:33 -0300

    feat: :sparkles: Now you can calculate the sha256 in a file

--- src/get/index.js
+++ src/get/index.js
@@ -2,10 +2,12 @@ const allFiles = require("./allFiles");
 const allFolders = require("./allFolders");
 const files = require("./files");
 const folders = require("./folders");
+const sha256 = require("./sha256");
 
 module.exports = {
   allFiles,
   allFolders,
   files,
   folders,
+  sha256,
 };
