ed664aa328e41c4482303325f38aa061ea676c60
FedYou <federicochurruarin0@gmail.com>
2024-11-13 23:48:57 -0300

    fix: :bug: The values for reading json were changed correctly

--- src/read/json.js
+++ src/read/json.js
@@ -1,27 +1,10 @@
 const file = require("./file");
-const regex = /("([^"\\]|\\.)*"|'([^'\\]|\\.)*'|\/\/.*|\/\*[\s\S]*?\*\/)/g;
+
 /**
- * Returns the contents of the file in an object, allows reading json with comments.
+ * Returns the contents of the file as a object.
  * @param {string} path - File path.
  * @returns {object}
  */
 module.exports = (path) => {
-  try {
-    let content = file(path);
-    if (content.charCodeAt(0) === 0xfeff) {
-      content = content.slice(1);
-    }
-
-    return JSON.parse(
-      content.replace(regex, (match) => {
-        if (match.startsWith('"') || match.startsWith("'")) {
-          return match;
-        }
-        return "";
-      })
-    );
-  } catch (error) {
-    console.log(path);
-    console.log(error);
-  }
+  return JSON.parse(file(path));
 };
