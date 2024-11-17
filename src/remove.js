fa9e43266a0c84e8b7e34976acdd6fda6f1bbdbb
FedYou <federicochurruarin0@gmail.com>
2024-11-17 01:04:23 -0300

    docs: JsDoc Update

--- src/remove.js
+++ src/remove.js
@@ -3,7 +3,6 @@ let fs = require("fs-extra");
 /**
  * Deletes files and directories.
  * @param {string} path - Directory or file path.
- * @param {string} dest - Destination path of the directory or file.
  */
 module.exports = (path) => {
   fs.removeSync(path);
--- src/removeExists.js
+++ src/removeExists.js
@@ -1,5 +1,9 @@
 const fs = require("fs-extra");
 
+/**
+ * Deletes files and directories if they exist.
+ * @param {string} path - Directory or file path.
+ */
 module.exports = (path) => {
   if (!fs.existsSync(path)) return;
   fs.removeSync(path);
