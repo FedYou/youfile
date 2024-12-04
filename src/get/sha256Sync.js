7052776e3892a2e951be7cdfc56ab83ad36ca730
FedYou <federicochurruarin0@gmail.com>
2024-12-04 01:43:35 -0300

    fix: :bug: Now sha256Sync is correct

--- src/get/sha256Sync.js
+++ src/get/sha256Sync.js
@@ -2,12 +2,8 @@ const crypto = require('crypto')
 const fs = require('fs')
 
 module.exports = (filePath) => {
-  return new Promise((resolve, reject) => {
-    const hash = crypto.createHash('sha256')
-    const stream = fs.createReadStream(filePath)
-
-    stream.on('data', (data) => hash.update(data))
-    stream.on('end', () => resolve(hash.digest('hex')))
-    stream.on('error', (err) => reject(err))
-  })
+  const hash = crypto.createHash('sha256')
+  const data = fs.readFileSync(filePath)
+  hash.update(data)
+  return hash.digest('hex')
 }
